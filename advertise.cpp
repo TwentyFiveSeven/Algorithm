646af8ab29df562ebc95778fc1428e6b4a14f9e4
JinGu <xortm854@naver.com>
2019-03-05 23:31:15 +0900

    remake algorithm

--- advertise.cpp
+++ advertise.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
-
+#include <vector>
 using namespace std;
 
-int main(){
-  long long int size,i;
-  int scount=0,save=0,ans=1;
-  char p;
+vector<int> fail(string p){
+  int i=1,j=0;
+  int m = p.size();
+  vector<int> f(m,0);
 
-  scanf("%lld",&size);
-  scanf("%c",&p);
-  char *s = new char[size];
-  for(i=0; i<size;i++){
-    scanf("%c",&p);
-    if(s[scount]==p){
-      s[i]=p;
-      ++scount;
-      scount =scount%ans;
-      save++;
+  while(i<m){
+    if(p[i]==p[j]){
+      f[i++]=(j++);
+    }else if(j>0){
+      j=f[j-1];
     }else{
-      if(i!=0){ans++;}
-      if(p!=s[0]||i!=size-1){ans = save+ans;}
-      s[i]=p;
-      scount=0;
-      save =0;
+      f[i++]=0;
     }
-    printf("%d %d %d\n",scount,ans,save);
   }
-  printf("%d\n",ans);
+  return f;
+}
+
+int kmp(string p){
+  int i,j,ans;
+  int m = p.size();
+  vector<int> f(m,0);
+
+  for(i=0;i<m;i++){
+    if(f[i]>0){
+      if(f[i]==1){
+        if(i==m-1){
+          ans = i;
+          break;
+        }else if(f[i]-f[i+1]!=-1){
+          
+        }
+      }
+    }
+  }
+  return ans;
+}
+
+int main(){
+  int size,i,ans;
+  string p;
+
+  getline(cin, p);
+  ans = kmp(p);
+  printf("%d",ans);
 
 }
