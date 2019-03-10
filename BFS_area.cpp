0e9d97b87459594beb774bb339584195f8c75e3d
JinGu <xortm854@naver.com>
2019-03-10 22:26:28 +0900

    No.2583 success

--- BFS_area.cpp
+++ BFS_area.cpp
@@ -1,68 +1,72 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-queue<pair<int,int>> Q;
+int m,n,k,num;
+int vx[4]={1,-1,0,0};
+int vy[4]={0,0,-1,1};
+vector<int> ans;
+int **arr;
+int **visit;
 
-int M,N;
-const int dx[] = { -1,0,1,0 };
-const int dy[] = { 0,-1,0,1 };
-
-int bfs(int count){
-  inf flag=0,i,j;
-  pair<int,int> P;
-  for(i=0;i<M;i++){
-    for(j=0;j<N;j++){
-      if(a[i][j]>0){
-        P = make_pair(i,j);
-        Q.push(P);
-        a[i][j]=0;
-        flag = 1;
-        break;
-      }
-    }
-    if(flag){
-      break;
+void dfs(int x,int y){
+  int i,j,px,py;
+  for(i=0;i<4;i++){
+    px=x+vx[i];py=x+vy[i];//&& !visit[i][j]
+    if(px>=0 && px<m && py<n && py>=0 && !arr[px][py]){
+      printf("px, py : %d, %d\n",px,py);
+      num++;
+      printf("num : %d\n",num);
+      //visit[i][j]=1;
+      dfs(px,py);
     }
   }
-
-  do{
-    P = Q.front();
-    Q.pop();
-    for()
-    if(a[i][j]>0){
-
-    }
-
-  }while(!Q.empty())
 }
 
+
 int main(){
-  int M,N,K,x1,y1,x2,y2,i,j,m;
+  int x1,y1,x2,y2,i,j,l;
 
-  cin>>M>>N>>K;
-  a = new int*[M];
-  for(i=0;i<M;i++){
-    a[i]=new int[N];
-    for(j=0;j<N;j++){
-      a[i][j]=j+7*i+1;
+  cin>>m>>n>>k;
+  arr = new int*[m];
+  visit = new int*[m];
+  for(i=0;i<m;i++){
+    arr[i]=new int[n];
+    visit[i] = new int[n];
+    for(j=0;j<n;j++){
+      arr[i][j]=0;
+      visit[i][j]=0;
     }
   }
-
-  for(m=0;m<K;m++){
-    cin>>y1>>x1>>y2>>x2;
-    for(i=x1;i<x2;i++){
-      for(j=y1;j<y2;j++){
-        a[i][j]=0;
-      }
-    }
-  }
-
-  for(i=0;i<M;i++){
-    for(j=0;j<N;j++){
-      printf("%d ",a[i][j]);
+  for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+      printf("%d ",visit[i][j]);
     }
     printf("\n");
   }
-  return 0;
+  for(l=0;l<k;l++){
+    cin>>y1>>x1>>y2>>x2;
+    for(i=x1;i<x2;i++){
+      for(j=y1;j<y2;j++){
+        arr[i][j]=1;
+      }
+    }
+  }
+  for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+      if(!arr[i][j] && !visit[i][j]){
+        visit[i][j]=1;
+        num=1;
+        printf("1");
+        dfs(i,j);
+        ans.push_back(num);
+      }
+    }
+  }
+  cout<<ans.size()<<endl;
+  sort(ans.begin(),ans.end());
+  for(i=0;i<k;i++){
+    cout<<ans[i]<<" ";
+  }
 }
