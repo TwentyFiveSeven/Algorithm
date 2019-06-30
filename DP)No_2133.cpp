c76b7faca9e456fdc2987f315a5b7b29c4eba300
JinGu <xortm854@naver.com>
2019-07-01 00:54:16 +0900

    DP_타일(성공 ㅎ)

--- DP)No_2133.cpp
+++ DP)No_2133.cpp
@@ -1,27 +1,28 @@
 #include<iostream>
-
+#include<math.h>
 using namespace std;
 
 long long int dp[31];
+int casenum[31];
 
 int main(){
   int n=0,i,j;
 
   scanf("%d",&n);
 
-  dp[1] = 0;
-  dp[2] = 3;
-  dp[3]= 0;
-  if(!(n%2)){
-    for(i=4;i<=n;i++){
-      if(!(i%2)){
-        dp[i] = dp[i-2]*3+2;
-      }
-    }
-    printf("%lld",dp[n]);
-  }else{
+  if(n%2){
     printf("0");
+    return 0;
+  }
+  fill_n(casenum,31,2);
+  casenum[2]=3;dp[0]=1;
+
+  for(i=2;i<=n;i=(i+2)){
+    for(j=2;j<=i;j=(j+2)){
+      dp[i] += dp[i-j]*casenum[j];  // 만약 n = 6일 경우, (6-2)*2, (6-4)*4, (6-6)*6 의 경우의 수를 구해야하고, 좌측에 있는 경수의 수 (6-2)일경우 구할 수 있는
+    }                               // 모든 경우의 수 ex)4 -> 2*2, 4 = 11 의 값이고, 우측 경우의 수는 4개가 독릭적으로 갖을 수 있는 경우의 수 2 이다.
   }
 
+  printf("%lld",dp[n]);
   return 0;
 }
