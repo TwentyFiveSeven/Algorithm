4d6ebac8ac4e82641946c88a60bcaf5a14070425
JinGu <xortm854@naver.com>
2019-07-02 00:15:13 +0900

    DFS,BFS_DFS와BFS(코드수정 및 주석추가)

--- BFS)No_1260.cpp
+++ BFS)No_1260.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int arr[1001][1001];
 int check[1001];
+int check2[1001];
 list<int> L;
 queue<int> Q;
 int n,m;
@@ -20,13 +21,29 @@ void bfs(int k){
     for(j=1;j<=n;j++){
       if(arr[tmp][j]&&!check[j]){
         Q.push(j);
-        check[j]=1;
+        check[j]=1;   //탐색하는 과정에서 Q에 push해줄 때 check를 해준다.
       }
     }
   }
 }
 
-void dfs(){
+void dfs(int k){
+  int i,j,tmp=0,count=0;
+  L.push_front(k);
+
+  while(!L.empty()){
+    tmp = L.front();
+    if(!check2[tmp]){   //정점을 지나가지않았을 때만 출력 후 check를 해준다. (중요)
+      printf("%d ",L.front());
+      check2[tmp]=1;
+    }
+    L.pop_front();
+    for(j=n;j>=1;j--){
+      if(arr[tmp][j]&&!check2[j]){
+        L.push_front(j);            //탐색하는 과정에서 List에 Push를 해줄 때 check를 함께 하지않는다.(현재 위치를 최우선적으로 고려하기위함)
+      }
+    }
+  }
 }
 
 int main(){
@@ -39,5 +56,9 @@ int main(){
     arr[p1][p2] = 1;
     arr[p2][p1] = 1;
   }
+  dfs(k);
+  printf("\n");
   bfs(k);
+
+  return 0;
 }
