7182d47e91ff4a947db224c5d08cd9f947e31395
JinGu <xortm854@naver.com>
2019-07-16 22:56:13 +0900

    Back_스도쿠

---
+++ Back)No_2580.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int arr[10][10];
+int Gans[10];
+int Sans[10];
+int Mans[10];
+
+
+
+int main(){
+  int i,j;
+  for(i=1;i<=9;i++){
+    for(j=1;j<=9;j++){
+      scanf("%d",&arr[i][j]);
+    }
+  }
+
+}
