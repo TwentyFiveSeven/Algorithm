86a697ea06e7d441e26082b7f37776c72f39aa66
JinGu <xortm854@naver.com>
2019-07-22 21:56:03 +0900

    Back_비숍(코드수정/주석추가)

--- Back)No_1799.cpp
+++ Back)No_1799.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-int R[20][2],L[20][2];
-int n,arr[11][11],Q[101],m=1,maxN;
+int R[20][2],L[20][2];  //대각선으로 최대 20x20으로 바둑판을 나눈다.
+int n,arr[11][11],BQ[101],WQ[101],wm=1,bm=1,maxN; //BQ는 black의 위치를 저장 WQ는 white의 위치 저장.
 
 int promising(int right, int left){
   if(R[right][1] || L[left][1])
@@ -11,7 +11,7 @@ int promising(int right, int left){
   return 1;
 }
 
-void chess(int count,int k){
+void chess(int Q[],int count,int k,int m){
   int x,y,i,j,right,left;
   if(maxN<count)
     maxN = count;
@@ -22,24 +22,21 @@ void chess(int count,int k){
   right = x+y-1;
   left = x-y+5;
 
-  if(promising(right,left)){
-    arr[x][y] = 2;
-    printf("k : %d, i : %d, x : %d, y : %d ==> In !\n",k,i,x,y);
-    R[right][1] = L[left][1] = 1;
-    chess(count+1,k+1);
-    if(R[right][0]<2&&L[left][0]<2)
-      return;
+  if(promising(right,left)){  //오른쪽 대각선, 왼쪽 대각선의 번호를 넘겨주어 해당 대각선들에 이미 바둑판이 있는지 확인.
+    arr[x][y] = 2;  //유망하다면 2로 위치를 표시.
+    R[right][1] = L[left][1] = 1; //현재 말의 우대각 좌대각에다가 말이 있음을 표시.
+    chess(Q,count+1,k+1,m); // 현재 count+1 , k+1을 통해 다음 위치의 유망함을 확인한다.
     arr[x][y] = 1;
     R[right][1] = L[left][1] = 0;
-    chess(count,k+1);
-  }else{
-    printf("k : %d, i : %d, x : %d, y : %d ==> Out !\n",k,i,x,y);
-    chess(count,k+1);
+    chess(Q,count,k+1,m); //현재 자리가 유망하지만, 현재 말을 두지않고 다음말을 두었을 떄를 확인.
+  }
+  else{
+    chess(Q,count,k+1,m); //유망하지않을 때 다음말로 가기 위한 재귀
   }
 }
 
 int main(){
-  int i,j,count=0,tmp=0,left,right;
+  int i,j,count=0,tmp=0,left,right,sum =0;
 
   scanf("%d",&n);
 
@@ -48,15 +45,20 @@ int main(){
       scanf("%d",&tmp);
       arr[i][j] = tmp;
       if(tmp){
-        Q[m++] = i*n+j-1;
-        right = i+j-1;
-        left = i-j+5;
-        R[right][0]++;L[left][0]++;
+        if((i+j)%2){       //만약 말의 위치가 white(even)이면 WQ에 저장.
+          WQ[wm++] = i*n+j-1;
+        }else{    //만약 말의 위치가 black(odd)이면 BQ에 저장.
+          BQ[bm++] = i*n+j-1;
+        }
       }
     }
   }
-  chess(0,1);
-  printf("%d",maxN);
+  chess(WQ,0,1,wm); //white위에 있는 것을 먼저 탐색.
+  sum = maxN;
+  maxN =0;
+  chess(BQ,0,1,bm); //다음 black에 있는 것을 먼저 탐색.
+  sum += maxN;
+  printf("%d",sum);
 
   return 0;
 }
