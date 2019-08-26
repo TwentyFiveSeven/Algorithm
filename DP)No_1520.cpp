eb90ba00c6a83d2aecec39bd1dba13b4dcf87a6c
JinGu <xortm854@naver.com>
2019-08-26 16:51:57 +0900

    DP_내리막길(코드수정/주석추가)

--- DP)No_1520.cpp
+++ DP)No_1520.cpp
@@ -1,26 +1,22 @@
-#include<iostream>
 #include<cstdio>
-// #include<queue>
-// #include<vector>
 using namespace std;
 int arr[500][500],N,M,dp[500][500];
-// queue<pair<int,int>> Q;
-// vector<pair<int,int>> V;
 int xi[4] = {0,0,1,-1};
 int yi[4] = {1,-1,0,0};
-int DFS(int x,int y){
+
+int DFS(int x,int y){ //DFS,BFS만으로는 재방문 알고리즘에 큰 어려움이 있기 때문에 재귀(check)를 통해 효과적인 방문.
   if(x == 0 && y ==0) return 1;
-  if(dp[x][y] != -1) return dp[x][y];
-  int ret = 0;
+  if(dp[x][y] != -1) return dp[x][y]; //dp를 이용해 check를 대신한다.
+  dp[x][y] = 0;
   for(int i=0;i<4;i++){
     int nx = x+xi[i];
     int ny = y+yi[i];
     if(nx>=0&&nx<N&&ny>=0&&ny<M)
       if(arr[nx][ny]>arr[x][y]){
-        ret += DFS(nx,ny);
+        dp[x][y] += DFS(nx,ny);
       }
   }
-  return ret;
+  return dp[x][y];
 }
 
 int main(){
@@ -32,6 +28,6 @@ int main(){
       dp[i][j] = -1;
     }
   }
-  // ans = BFS();
   printf("%d",DFS(N-1,M-1));
+  return 0;
 }
