07ecab16a9f89d65544b9930c2bbc1a8e2293fbc
JinGu <xortm854@naver.com>
2019-08-29 00:40:03 +0900

    DP,LIS_꼬인 전깃줄(LIS의한계 lower_bound로 극복)

---
+++ DP_LIS)No_1365.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int arr[100000],N;
+vector<int> V;
+int main(){
+  int i,j;
+  scanf("%d",&N);
+  for(i=0;i<N;i++){
+    scanf("%d",&arr[i]);
+  }
+  for(i=0;i<N;i++){
+    int idx = lower_bound(V.begin(),V.end(),arr[i])-V.begin(); //-V.begin() 을 통해서 몇번 째 인자인지 알 수 있다
+    if(idx == V.size()){  //인자와 V.size()가 같다는 말은 arr[i]가 V안에 없다는 것 = arr[i]가 V안에 수들보다 크다는 것
+      V.push_back(arr[i]);  // 이말은 즉 count가 된다 이므로 push한다.
+    }else{
+      V[idx] = arr[i]; //같지 않다는 것은 V안에 있는 수 중에 arr[i]로 더작은 수를 저장 할 수 있다는 것이다.
+    }
+  }
+  printf("%d",N-V.size());
+  return 0;
+}
