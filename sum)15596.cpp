f50445f7ca9a525cc2e1beac443615ad1e3515da
JinGu <xortm854@naver.com>
2020-01-07 00:01:43 +0900

    sum

---
+++ sum)15596.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+long long sum(vector<int> &a){
+  long long ret = 0;
+  int size = a.size();
+  for(int i=0;i<size;i++)
+    ans += a[i];
+  return 0;
+}
