dd6e954b134e13d2aafb75b21451a54f6c25bc6f
JinGu <xortm854@naver.com>
2020-02-07 22:39:29 +0900

    Backtracking_N과 M(3)

---
+++ Back)No_15651.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+const int MAX = 9;
+int n,m;
+int arr[MAX];
+void func(int count){
+  if(count == m){
+    for(int i=0;i<m;i++)
+      printf("%d ",arr[i]);
+    printf("\n");
+    return;
+  }
+
+  for(int i=1;i<=n;i++){
+    arr[count] = i;
+    func(count+1);
+  }
+}
+
+int main(){
+  scanf("%d %d",&n,&m);
+  func(0);
+  return 0;
+}
