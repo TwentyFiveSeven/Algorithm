88870d5f609ab723d420f722a2af854e8c4f8c79
JinGu <xortm854@naver.com>
2020-02-10 23:12:47 +0900

    Math_3053

---
+++ Math)No_3053.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  double k = n;
+  printf("%0.6lf\n",k * k *3.14159265358979);
+  printf("%0.6lf",k*k*2);
+  return 0;
+}
