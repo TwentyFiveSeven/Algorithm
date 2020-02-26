08a1eb7a8fde31feda2c6a5f6c45475c2484aca9
JinGu <xortm854@naver.com>
2020-02-26 23:46:37 +0900

    DP_연속합(코드수정)

--- DP)No_1912.cpp
+++ DP)No_1912.cpp
@@ -1,24 +1,18 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int arr[100001],dp[100001],save=0,first=0;
+int arr[100001],value,Maxvalue;
 int main(){
   int n;
   scanf("%d",&n);
   for(int i=0;i<n;i++){
     scanf("%d",&arr[i]);
   }
-  dp[0] = arr[0];
-  first = 0;
+  value = Maxvalue = arr[0];
   for(int i=1;i<n;i++){
-    dp[i] = max({dp[i-1],first+arr[i]});
-
-    if(arr[i]<0) first = 0;
-    else first += arr[i];
-    printf("dp[i-1] : %d, first+arr[i] : %d, first : %d, dp[i] : %d\n",dp[i-1],first+arr[i],first,dp[i]);
+    value = max(arr[i],arr[i]+value);
+    Maxvalue = max(Maxvalue,value);
   }
-  for(int i=0;i<n;i++){
-    printf("%d ",dp[i]);
-  }
+  printf("%d",Maxvalue);
   return 0;
 }
