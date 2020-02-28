f7bcf3189bbe05a367d144107b81d1daba48deaf
JinGu <xortm854@naver.com>
2020-02-28 23:50:20 +0900

    Math_이항계수

---
+++ Math)No_11050.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int arr[12];
+int main(){
+  int n,k;
+  scanf("%d %d",&n,&k);
+  arr[1] = arr[2] = 1;
+  for(int i=2;i<=n;i++){
+    for(int j=i+1;j>=1;j--){
+      arr[j] = arr[j]+arr[j-1];
+    }
+  }
+  printf("%d",arr[k+1]);
+  return 0;
+}
