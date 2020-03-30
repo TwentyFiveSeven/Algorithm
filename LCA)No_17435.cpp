c7603e758d09449efeb314a277f62f86abab1fb7
JinGu <xortm854@naver.com>
2020-03-30 12:23:27 +0900

    LCA_합성함수와 쿼리

---
+++ LCA)No_17435.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int arr[200001],p[200001];
+int main(){
+  int M,Q,a,b;
+  cin>>M;
+  for(int i=1;i<=M;i++){
+    cin>>arr[i];
+  }
+  for(int i=1;i<=M;i++){
+    p[i] = arr[arr[i]];
+  }
+  cin>>Q;
+  for(int i=0;i<Q;i++){
+    cin>>a>>b;
+    b
+  }
+}
