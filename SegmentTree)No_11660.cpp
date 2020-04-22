1066448dc992001087245989a8f7a886b6db23d0
JinGu <xortm854@naver.com>
2020-04-22 23:08:44 +0900

    SegmentTree_구간 합 구하기5

---
+++ SegmentTree)No_11660.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int arr[1048576],segT[2097152];
+
+int makeTree(int index, int start, int end){
+  if(start == end)
+    return segT[index] = arr[start];
+  return segT[index] = makeTree(index*2+1,start,(start+end)/2)+makeTree(index*2+2,(start+end)/2+1,end);
+}
+
+int query(int l,int r,int index,int start,int end){
+  if(l > end || r <start)
+    return 0;
+  if(start >=l && end <=r)
+    return segT[index];
+  return query(l,r,index*2+1,start,(start+end)/2)+query(l,r,index*2+2,(start+end)/2+1,end);
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int N,M,x1,x2,y1,y2;
+  cin>>N>>M;
+  for(int i=0;i<N;i++){
+    for(int j=0;j<N;j++){
+      cin>>arr[i*N+j];
+    }
+  }
+  makeTree(0,0,N*N-1);
+  int sum;
+  for(int i=0;i<M;i++){
+    sum=0;
+    cin>>x1>>y1>>x2>>y2;
+    for(int j=x1-1;j<=x2-1;j++){
+      int nx = (j)*N+y1-1;
+      int ny = (j)*N+y2-1;
+      sum += query(nx,ny,0,0,N*N-1);
+    }
+    cout<<sum<<'\n';
+  }
+  return 0;
+}
